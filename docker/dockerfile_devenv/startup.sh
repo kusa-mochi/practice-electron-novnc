240f84fd510072919550382e676a901786efb555
kusa-mochi <whoatemyapplepie@gmail.com>
2024-09-13 22:10:32 +0900

    make a shell file to run AppImage executable file.

---
+++ docker/dockerfile_devenv/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf /tmp/app
+mkdir -p /tmp/app
+cp /project/electron-app/dist/electron-app-1.0.0.AppImage /tmp/app/
+cd /tmp/app
+./electron-app-1.0.0.AppImage --appimage-extract-and-run --no-sandbox --touch-events=enabled
